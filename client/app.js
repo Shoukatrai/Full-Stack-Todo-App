e1570f79dbd229c4bd329c9e1899a2edc7ba1866
Shoukatrai <raishoukat033@gmail.com>
2025-03-17 15:03:28 +0500

    crud operation from client side with proper UI

--- client/app.js
+++ client/app.js
@@ -3,38 +3,122 @@ const inputTodo = document.querySelector("#inputTodo")
 
 console.log(todosContainer)
 
-const todoGet = ()=>{
+const todoGet = () => {
     console.log("todoGet")
     fetch("http://localhost:3030/todoget")
-    .then((res)=>res.json())
-    .then((todos)=>{
-        console.log("todos" , todos.message)
-        const allTodos = todos.response
-        for(var i =0 ; i <allTodos.length - 1; i++){
-            const todoId= allTodos[i]._id
-            const todoTask= allTodos[i].newTask
-           
-            todosContainer.innerHTML += `  <span> ${todoTask} </span>
-            <button>Edit</button>
-            <button>Delete</button>`
-        }
-    })
-    .catch((err)=>{
-        console.log("error" , err)
-    })
+        .then((res) => res.json())
+        .then((todos) => {
+            console.log("todos", todos.message)
+            const allTodos = todos.response
+            for (var i = 0; i < allTodos.length; i++) {
+                const todoId = allTodos[i]._id
+                const todoTask = allTodos[i].newTask
+                const todoEle = document.createElement("div")
+                todoEle.innerHTML += `
+            <span> ${todoTask} </span>
+            <div>
+                <button id = "${todoId}" onclick = "todoEdit(this)">
+                    <img src="./images/editing.png" alt="">
+                </button>
+                <button id = "${todoId}" onclick = "todoDelete(this)">
+                    <img src="./images/remove.png" alt="">
+                </button>`
+                todosContainer.appendChild(todoEle)
+            }
+        })
+        .catch((err) => {
+            console.log("error", err)
+        })
 }
 
-const todoCreate = ()=>{
+const todoCreate = () => {
     console.log("todoGet")
-    const data = {
-        newTask : inputTodo.value
+    if (inputTodo.value.length < 3) {
+        alert("Please Enter any todo")
+        return
     }
-    fetch("http://localhost:3030/todocreate" ,{
-        method : "POST",
-        headers : {
-            "Content-Type" : "application/json"
+    const data = {
+        newTask: inputTodo.value
+    }
+    fetch("http://localhost:3030/todocreate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-        body : JSON.stringify(data)
-    } )
-    
+        body: JSON.stringify(data)
+    })
+        .then((response) => {
+            console.log("response", response)
+            alert("Todo Created Successfully!")
+            todosContainer.innerHTML = "" // todosContainer empty
+            todoGet() //refresh the UI
+        })
+        .catch((error) => {
+            console.log("error", error)
+            alert("error", error.code)
+        })
+
 }
+
+
+
+
+const todoEdit = (ele) => {
+    console.log("todoGet")
+    const eleId = ele.id
+    const editPrompt = prompt("Enter value!")
+    if (editPrompt == null) {
+        alert("Please Enter any todo")
+        return
+    }
+    const data = {
+        newTask: editPrompt
+    }
+    fetch(`http://localhost:3030/todoupdate/${eleId}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then((response) => {
+            console.log("response", response)
+            alert("Todo Updated Successfully!")
+            todosContainer.innerHTML = "" // todosContainer empty
+            todoGet() //refresh the UI
+        })
+        .catch((error) => {
+            console.log("error", error)
+            alert("error", error.code)
+        })
+}
+
+
+
+
+const todoDelete = (ele) => {
+    const eleId = ele.id
+    const deletPrompt = confirm("Are you sure!")
+
+    if (deletPrompt) {
+        fetch(`http://localhost:3030/tododelete/${eleId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((response) => {
+                console.log("response", response)
+                alert("Todo Deleted Successfully!")
+                todosContainer.innerHTML = "" // todosContainer empty
+                todoGet() //refresh the UI
+            })
+            .catch((error) => {
+                console.log("error", error)
+                alert("error", error.code)
+            })
+    }else{
+        return
+    }
+}
+
